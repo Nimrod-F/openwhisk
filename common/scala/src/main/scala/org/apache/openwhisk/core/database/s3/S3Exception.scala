31e31deff924f4523ca854ca4b39efaec3cc25de
PJ Fanning <pjfanning@users.noreply.github.com>
2024-05-27 13:01:06 +0100

    scalafmt

--- common/scala/src/main/scala/org/apache/openwhisk/core/database/s3/S3Exception.scala
+++ common/scala/src/main/scala/org/apache/openwhisk/core/database/s3/S3Exception.scala
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
- /*
+/*
  * Copyright (C) 2016-2019 Lightbend Inc. <http://www.lightbend.com>
  */
 
@@ -25,25 +25,24 @@ import scala.util.Try
 import scala.xml.{Elem, XML}
 
 /**
-  * Exception thrown by S3 operations.
-  *
-  * Copied from https://github.com/akka/alpakka/blob/v1.0.2/s3/src/main/scala/akka/stream/alpakka/s3/S3Exception.scala
-  */
+ * Exception thrown by S3 operations.
+ *
+ * Copied from https://github.com/akka/alpakka/blob/v1.0.2/s3/src/main/scala/akka/stream/alpakka/s3/S3Exception.scala
+ */
 private[s3] class S3Exception(val code: String, val message: String, val requestId: String, val hostId: String)
     extends RuntimeException(message) {
 
   def this(xmlResponse: Elem) =
-    this((xmlResponse \ "Code").text,
-         (xmlResponse \ "Message").text,
-         (xmlResponse \ "RequestID").text,
-         (xmlResponse \ "HostID").text)
+    this(
+      (xmlResponse \ "Code").text,
+      (xmlResponse \ "Message").text,
+      (xmlResponse \ "RequestID").text,
+      (xmlResponse \ "HostID").text)
 
   def this(response: String) =
     this(
       Try(XML.loadString(response)).getOrElse(
-        <Error><Code>-</Code><Message>{response}</Message><RequestID>-</RequestID><HostID>-</HostID></Error>
-      )
-    )
+        <Error><Code>-</Code><Message>{response}</Message><RequestID>-</RequestID><HostID>-</HostID></Error>))
 
   override def toString: String =
     s"${super.toString} (Code: $code, RequestID: $requestId, HostID: $hostId)"
